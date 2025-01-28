4320b720d5c6668027d3566c97ea31d6340b7dea
Thomas HODSON <thomas.hodson.pro@gmail.com>
2025-01-28 18:33:10 +0100

    Sort a string challenge

--- sort_a_string/main.py
+++ sort_a_string/main.py
@@ -1,7 +1,5 @@
 def sort_string_by_words(string: str) -> str:
-    words = string.split()
-    words = sorted(words, key=lambda w : w.lower())
-    return " ".join(words)
+    return " ".join(sorted(string.split(), key=lambda w : w.lower()))
 
 if __name__ == "__main__":
     assert sort_string_by_words("strings of words") == "of strings words"
